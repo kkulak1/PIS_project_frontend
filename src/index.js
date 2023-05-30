4d9152a4f9368a02bac115826f17cc081088e864
Corvus <olekolek@spoko.pl>
2023-05-31 01:24:24 +0200

    finished preparation for login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, Outlet, RouterProvider, Navigate } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import RouteCreator from './RouteCreator.jsx'
+import { 
+  textRoot, 
+  logIn, 
+  backendAddress
+} from './utils/pathutils'
+import { theme } from './theme'
+
+
+import LogInPage from './pages/LogInPage';
+
+
+let routeCreator = new RouteCreator(
+  backendAddress,
+  <Navigate to={logIn}/>
+)
+
+const router = createBrowserRouter(
+  [{
+    path: textRoot,
+    element: (
+      <ThemeProvider theme={theme}>
+        <Outlet/>
+      </ThemeProvider>
+    ),
+    children: [
+      routeCreator.createRoute(textRoot, <Navigate to={logIn}/>),
+      routeCreator.createRoute(logIn, <LogInPage/>),
+    ]
+  }]
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
 
