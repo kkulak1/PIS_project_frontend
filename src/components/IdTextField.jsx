bf68bca9f3e8da1c49712460940451e15866055f
Corvus <olekolek@spoko.pl>
2023-05-31 02:45:58 +0200

    added wrapper for text field

---
+++ src/components/IdTextField.jsx
@@ -0,0 +1,15 @@
+import { TextField } from "@mui/material"
+
+
+export function IdTextField(label, value, onChange, type="text", id=null) {
+  id = id ?? `${label.toLowerCase()}-text-field`
+  return (
+  <TextField 
+    id={id}
+    label={label}
+    sx={{mt: 1}}
+    value={value}
+    type={type}
+    onChange={onChange}
+  />)
+}
