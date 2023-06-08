2f1b65b794b49a25563f411be4394d40b632a4f6
Corvus <olekolek@spoko.pl>
2023-06-09 00:24:25 +0200

    added template

---
+++ src/pages/overviewpage/operations.jsx
@@ -0,0 +1,25 @@
+
+import * as restutils from "../../utils/restutils"
+import { backendAddress, logIn } from "../../utils/pathutils"
+
+export async function postRegisterData(login, password) {
+const responseJson = await restutils.postJson(
+    backendAddress + logIn,
+    JSON.stringify({
+    'login': login,
+    'password': password
+    })
+)
+return responseJson;
+}
+
+
+export function logSuccess(responseJson) {
+  console.log('Logged in');
+  console.log('Role: ' + responseJson.role);
+}
+
+export function logFailure() {
+  console.log('Failed to log in');
+}
+
