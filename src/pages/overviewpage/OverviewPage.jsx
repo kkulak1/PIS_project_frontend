eff3641cdc6be4ba85f1d128253f2fb444ffba76
Corvus <olekolek@spoko.pl>
2023-06-10 11:14:42 +0200

    added table pagination

--- src/pages/overviewpage/OverviewPage.jsx
+++ src/pages/overviewpage/OverviewPage.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import OverviewAppBar from "../../components/appbar/OverviewAppBar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Table, TableBody, TableCell, TableRow, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import { Box } from '@mui/system';
+import { Button, Table, TableBody, TableCell, TableRow, FormControl, InputLabel, Select, MenuItem, Pagination, TableContainer, Paper, TableHead, TablePagination} from '@mui/material';
+import { Box, margin } from '@mui/system';
 import EditButton from "../../components/EditButton"
 import DeleteButton from "../../components/DeleteButton"
 import { placeTableHeader } from "../../utils/placementutils";
 
 const OverviewPage = () => {
   const [pagination, setPagination] = useState(10); 
+  const [page, setPage] = useState(0);
+  const startIndex = page * pagination;
+  const endIndex = startIndex + pagination;
+  const recipes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+  const currentPageRows = recipes.slice(startIndex, endIndex);
+
 
   const handleCreateClick = () => {
   };
@@ -25,32 +31,10 @@ const OverviewPage = () => {
     )
   }
 
-  const handlePaginationChange = (event) => {
-    setPagination(Number(event.target.value));
-  };
-
-  // function PaginationItem({ pagination }) {
-  //   let stringNumber = String(pagination);
-  //   return (
-  //     <MenuItem value={pagination}>{stringNumber}</MenuItem>
-  //   )
-  // }
-
-  let recipes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-  let paginationLabel = 'pagination-label'
-  
-  return (
-    <Box marginTop={2} marginX={20}>
-      <OverviewAppBar/>
-      <Button 
-        variant="contained" 
-        color="primary" 
-        onClick={handleCreateClick} 
-        sx={{marginRight: '10px'}}
-      >
-        Create new recipe
-      </Button>
-      <FormControl sx={{ width: '280px' }}>
+  function PaginationSelector() {
+    let paginationLabel = 'pagination-label'
+    return (
+      <FormControl sx={{ width: '280px', margin: '10px' }}>
         <InputLabel id={paginationLabel}>Number of Displayed Recipes</InputLabel>
         <Select
           labelId={paginationLabel}
@@ -62,28 +46,72 @@ const OverviewPage = () => {
           <MenuItem value={10}>10</MenuItem>
           <MenuItem value={20}>20</MenuItem>
           <MenuItem value={30}>30</MenuItem>
-          {/* <PaginationItem pagination={20}/> */}
         </Select>
       </FormControl>
+    )
+  }
 
-      <Table>
-        <OverviewPageHeaderRow/>
-        <TableBody>
-          {recipes.slice(0, pagination).map((recipe) => (
-            <TableRow key={recipe}>
-              <TableCell>{recipe}</TableCell>
-              <TableCell>{recipe}</TableCell>
-              <TableCell>{recipe}</TableCell>
-              <TableCell>{recipe}</TableCell>
-              <TableCell>{recipe}</TableCell>
-              <TableCell>
-                <EditButton onClick={() => handleEditClick(recipe)} />
-                <DeleteButton onClick={() => handleDeleteClick(recipe)} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+  function CreateNewRecipeButton() {
+    return (
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={handleCreateClick} 
+        sx={{marginRight: '10px', marginBottom: '10px'}}
+      >
+        Create new recipe
+      </Button>
+    )
+  }
+
+
+  const handlePaginationChange = (event) => {
+    // setPagination(Number(event.target.value));
+    setPagination(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+
+  return (
+    <Box marginTop={2} marginX={20}>
+      <OverviewAppBar/>
+      <CreateNewRecipeButton/>
+      <TableContainer component={Paper}>
+        <Table>
+          <OverviewPageHeaderRow/>
+          <TableBody>
+            {currentPageRows.map((recipe) => (
+              <TableRow key={recipe}>
+                <TableCell>{recipe}</TableCell>
+                <TableCell>{recipe}</TableCell>
+                <TableCell>{recipe}</TableCell>
+                <TableCell>{recipe}</TableCell>
+                <TableCell>{recipe}</TableCell>
+                <TableCell>
+                  <EditButton onClick={() => handleEditClick(recipe)} />
+                  <DeleteButton onClick={() => handleDeleteClick(recipe)} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[10, 20, 30]}
+          component="div"
+          count={recipes.length}
+          rowsPerPage={pagination}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handlePaginationChange}
+        />
+      </TableContainer>
+      {/* <Pagination count={Math.ceil(recipes.length/pagination)} showFirstButton showLastButton sx={{margin:'px'}}/> */}
+      {/* <PaginationSelector/> */}
+      {/* <MyTable/> */}
     </Box>
   );
 };
