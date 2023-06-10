827303e9152cb5dfed2db85b8e00672e15b92071
Corvus <olekolek@spoko.pl>
2023-06-10 06:47:05 +0200

    removed unnecessary functions

--- src/utils/placementutils.jsx
+++ src/utils/placementutils.jsx
@@ -1,26 +1,10 @@
 
 import { 
-  Button, FormLabel, TextField, Grid, AppBar, 
-  Toolbar, Typography, 
+  Button, FormLabel, TextField, Grid,  
   TableBody, TableHead, TableRow, TableCell
 } from '@mui/material';
 import CenteringGrid from  '../components/CenteringGrid';
 
-export function SimpleAppBar() {
-  return (
-    <AppBar 
-    sx={{
-    color: 'black',
-    backgroundColor: 'silver',
-    mb: 1}}>
-        <Toolbar style={{ justifyContent: 'center' }}>
-            <Typography variant='h1'>
-                ProMan
-            </Typography>
-        </Toolbar>
-    </AppBar>
-  )
-}
 
 export function placeTextField(label, value, onChange, type="text", id=null) {
   id = id? id: `${label.toLowerCase()}-text-field`
