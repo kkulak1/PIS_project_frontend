2e01dc90e196c1b8eb3087797c4951e2609edbf8
Corvus <olekolek@spoko.pl>
2023-06-10 11:22:10 +0200

    created

---
+++ src/auxiliaryclasses/Quantity.jsx
@@ -0,0 +1,9 @@
+
+class Quantity {
+  quantityValue
+  quantityUnit
+  constructor(quantityValue, quantityUnit) {
+    this.amountOfTime = quantityValue;
+    this.quantityUnit = quantityUnit;
+  }
+}
