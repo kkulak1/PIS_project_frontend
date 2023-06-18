7c02fca00eeb9b36422fcb899989f927dc49bf57
Hubert <hubert.brzoskniewicz@gmail.com>
2023-06-18 12:27:47 +0200

    fetches without working token

--- src/pages/overviewpage/operations.jsx
+++ src/pages/overviewpage/operations.jsx
@@ -1,16 +1,198 @@
 
 import * as restutils from "../../utils/restutils"
-import { backendAddress, logIn } from "../../utils/pathutils"
+import { addCostBackend, addIngredientBackend, addQuantityBackend, addRecipeBackend, backendAddress, changeRecipeBackend, changeStepBackend, deleteCostBackend, deleteIngredientBackend, deleteQuantityBackend, deleteRecipeBackend, getRecipeBackend, getRecipeDetailsBackend, logIn, modifyCostBackend, modifyIngredientBackend, modifyQuantityBackend, putRecipeChangeDurationBackend } from "../../utils/pathutils"
 
-export async function postRegisterData(login, password) {
-const responseJson = await restutils.postJson(
-    backendAddress + logIn,
+//recipe
+export async function postAddRecipeData(name, description) {
+  const response = await restutils.postWithBody(
+    backendAddress + addRecipeBackend,
+    token,
     JSON.stringify({
-    'login': login,
-    'password': password
+      'name': name,
+      'description': description
     })
-)
-return responseJson;
+  )
+  return response;
+}
+
+export async function postRecipeChangeNameData(recipeId, newName) {
+  const response = await restutils.postWithBody(
+    backendAddress + changeRecipeBackend,
+    token,
+    JSON.stringify({
+      'recipeId': recipeId,
+      'description': description
+    })
+  )
+  return response;
+}
+
+export async function deleteRecipeData(id) {
+  const response = await restutils.deleteWithBody(
+    backendAddress + deleteRecipeBackend,
+    token,
+    JSON.stringify({
+      'id': id
+    })
+  )
+  return response;
+}
+
+export async function getRecipeData() {
+  const response = await restutils.get(
+    backendAddress + getRecipeBackend,
+    token
+  )
+  return response;
+}
+
+export async function getRecipeDetailsData(recipe) {
+  const response = await restutils.getWithBody(
+    backendAddress + getRecipeDetailsBackend,
+    token,
+    JSON.stringify({
+      recipe    //nie wiem co tutaj dac jak w request ma byc po prostu obiekt recipe
+    })
+  )
+  return response;
+}
+
+//change duration //moze nie dzialac cos
+export async function pusstRecipeChangeDurationData(recipe, newTime) {
+  const response = await restutils.putWithBody(
+    backendAddress + putRecipeChangeDurationBackend,
+    token,
+    JSON.stringify({
+      'recipe': recipe,
+      'newTime': newTime
+    })
+  )
+  return response;
+}
+
+//step
+export async function putStepChangeData(recipe, steps) {
+  const response = await restutils.putWithBody(
+    backendAddress + changeStepBackend,
+    token,
+    JSON.stringify({
+      'recipe': recipe,
+      'steps': steps
+    })
+  )
+  return response;
+}
+
+//quantity
+export async function addQuantityData(value, unit, ingredient) {
+  const response = await restutils.postWithBody(
+    backendAddress + addQuantityBackend,
+    token,
+    JSON.stringify({
+      'value': value,
+      'unit': unit,
+      'ingredient': ingredient
+    })
+  )
+  return response;
+}
+
+export async function deleteQuantityData(quantity) {
+  const response = await restutils.deleteWithBody(
+    backendAddress + deleteQuantityBackend,
+    token,
+    JSON.stringify({
+      quantity  //nie wiem co tutaj dac jak w request ma byc po prostu obiekt quantity
+    })
+  )
+  return response;
+}
+
+export async function modifyQuantityData(quantity) {
+  const response = await restutils.putWithBody(
+    backendAddress + modifyQuantityBackend,
+    token,
+    JSON.stringify({
+      quantity  //nie wiem co tutaj dac jak w request ma byc po prostu obiekt quantity
+    })
+  )
+  return response;
+}
+
+//ingredient
+export async function addIngredientData(recipe, type, name, description) {
+  const response = await restutils.postWithBody(
+    backendAddress + addIngredientBackend,
+    token,
+    JSON.stringify({
+      'recipe': recipe,
+      'type': type,
+      'name': name,
+      'description': description
+    })
+  )
+  return response;
+}
+
+export async function deleteIngredientData(id) {
+  const response = await restutils.deleteWithBody(
+    backendAddress + deleteIngredientBackend,
+    token,
+    JSON.stringify({
+      'id': id
+    })
+  )
+  return response;
+}
+
+export async function modifyIngredientData(id, type, name, description) {
+  const response = await restutils.putWithBody(
+    backendAddress + modifyIngredientBackend,
+    token,
+    JSON.stringify({
+      'id': id,
+      'type': type,
+      'name': name,
+      'description': description
+    })
+  )
+  return response;
+}
+
+//cost
+export async function addCostData(ingredient, value, unit) {
+  const response = await restutils.postWithBody(
+    backendAddress + addCostBackend,
+    token,
+    JSON.stringify({
+      'ingredient': ingredient,
+      'value': value,
+      'unit': unit,
+    })
+  )
+  return response;
+}
+
+export async function deleteCostData(cost) {
+  const response = await restutils.deleteWithBody(
+    backendAddress + deleteCostBackend,
+    token,
+    JSON.stringify({
+      cost  //nie wiem co tutaj dac jak w request ma byc po prostu obiekt cost
+    })
+  )
+  return response;
+}
+
+export async function modifyCostData(cost) {
+  const response = await restutils.putWithBody(
+    backendAddress + modifyCostBackend,
+    token,
+    JSON.stringify({
+      cost  //nie wiem co tutaj dac jak w request ma byc po prostu obiekt cost
+    })
+  )
+  return response;
 }
 
 
--- src/utils/pathutils.jsx
+++ src/utils/pathutils.jsx
@@ -12,6 +12,29 @@ export const registration = makePath("", "register");
 // export const registrationBackend = makePath("", "api", "v1", "auth", "register");
 export const registrationBackend = '/api/v1/auth/register';
 
+export const addRecipeBackend = '/api/v1/recipe/add';
+export const changeRecipeBackend = '/api/v1/recipe/change-name';
+export const deleteRecipeBackend = '/api/v1/recipe/delete';
+export const getRecipeBackend = '/api/v1/recipe/get';
+export const getRecipeDetailsBackend = '/api/v1/recipe/get-details';
+
+export const putRecipeChangeDurationBackend = '/api/v1/recipe/change-duration';
+
+export const changeStepBackend = '/api/v1/step/change-steps';
+
+export const addQuantityBackend = '/api/v1/quantity/add';
+export const deleteQuantityBackend = '/api/v1/quantity/delete';
+export const modifyQuantityBackend = '/api/v1/quantity/modify';
+
+export const addIngredientBackend = '/api/v1/ingredient/add';
+export const deleteIngredientBackend = '/api/v1/ingredient/delete';
+export const modifyIngredientBackend = '/api/v1/ingredient/modify';
+
+export const addCostBackend = '/api/v1/cost/add';
+export const deleteCostBackend = '/api/v1/cost/delete';
+export const modifyCostBackend = '/api/v1/cost/modify';
+
+
 export const overview = makePath("", "overview");
 
 export const textRoot = "/";
